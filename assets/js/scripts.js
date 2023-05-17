9a879d0ba17228c5012a805b626d5bd1ed0b4e0e
leansdr <leansdr@gmail.com>
2023-05-17 01:08:44 -0300

    Colocando API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,17 +4,6 @@ function changeDisplay() {
   overlay.style.display = "none";
 }
 
-const cartaoPokemons = document.querySelectorAll(".cartao-pokemon");
-cartaoPokemons.forEach((pokemon) => {
-  pokemon.addEventListener("click", () => {
-    if (overlay.classList.contains("animate__zoomOut")) {
-      overlay.classList.remove("animate__zoomOut");
-    }
-    overlay.classList.add("animate__zoomIn");
-    overlay.style.display = "flex";
-  });
-});
-
 const btnCloseModal = document.querySelector(".btn-close img");
 btnCloseModal.addEventListener("click", () => {
   if (overlay.classList.contains("animate__zoomIn")) {
@@ -24,15 +13,70 @@ btnCloseModal.addEventListener("click", () => {
   }
 });
 
-const url = "https://pokeapi.co/api/v2/pokemon?limit=10";
+//Selecionando os elemento do HTML
+let img = document.querySelector("#imagem");
+let nome = document.querySelector("#nome");
+let numero = document.querySelector("#numero");
+let containerPokemon = document.querySelector("#container");
 
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((erro) => {
-    console.log("Erro: " + erro);
+//Pegando as informações da API
+const pegarPokemon = async () => {
+  /*
+  const url = `https://pokeapi.co/api/v2/pokemon/1/`;
+  const resp = await fetch(url);
+  const data = await resp.json();
+  console.log(data.types);
+  */
+
+  const arrayPokemon = [];
+  for (let i = 1; i <= 251; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    arrayPokemon.push({
+      number: data.id,
+      img: data.sprites.other.dream_world.front_default,
+      name: data.name,
+      tipo: data.types,
+    });
+  }
+
+  listarPokemon(arrayPokemon);
+};
+
+//Listando as informações no Front End
+function listarPokemon(pokemons) {
+  pokemons.forEach((pokemon) => {
+    let card = `<li class="cartao-pokemon ${pokemon.tipo
+      .map((item) => " " + item.type.name)
+      .toString()
+      .replace(",", "")}">
+    <span id="numero">#${pokemon.number}</span>
+    <figure class="cartao-imagem">
+      <img
+        id="imagem"
+        src=${pokemon.img}
+        alt=${pokemon.name}
+      />
+    </figure>
+    <figcaption>
+      <h3 id="nome">${pokemon.name}</h3>
+    </figcaption>
+  </li>`;
+    containerPokemon.innerHTML += card;
   });
+}
+
+const cartaoPokemons = document.querySelectorAll(".cartao-pokemon");
+cartaoPokemons.forEach((pokemonCard) => {
+  pokemonCard.addEventListener("click", () => {
+    if (overlay.classList.contains("animate__zoomOut")) {
+      overlay.classList.remove("animate__zoomOut");
+    }
+    overlay.classList.add("animate__zoomIn");
+    overlay.style.display = "flex";
+  });
+});
+
+document.addEventListener("load", pegarPokemon());
